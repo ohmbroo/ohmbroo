f24f70b1ae86e32c121d0b634f3860118d418bbe
Edward Sadzewicz <ohmbroo@gmail.com>
2024-12-30 21:31:47 -0700

    Code cleanup and add e-mail in footer.

--- index.php
+++ index.php
@@ -4,15 +4,6 @@ To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
-<html>
-  <head>
-    <meta charset="UTF-8">
-          <title>OHMBROO</title>
-     <link rel="icon" href="rainbow.png">
-<!--     <link rel="icon" href="beer-2.svg"> -->
-<!--      <link rel="icon" href="rainbow-balloon.png"> -->
-<!--     <link rel="icon" href="darth-vader.png">     -->
-  </head>
 <?php
 ////////////////////////////////////////////////////////////////////////////////
 //
@@ -33,45 +24,52 @@ and open the template in the editor.
 //include_once 'config/lang_config.php';
 //include_once 'translations/' . $lang_file;
 ?>
-<!DOCTYPE html>
 <html>
+<head>
+<title>OHMBROO</title>
+<meta charset="UTF-8">
+<link rel="icon" href="rainbow.png">
+<!--     <link rel="icon" href="beer-2.svg"> -->
+<!--      <link rel="icon" href="rainbow-balloon.png"> -->
+<!--     <link rel="icon" href="darth-vader.png">     -->
  
-  <style>
-    #nav {
-          line-height:30px;
-          height:800px;
-          width:100px;
-          float:left;
-          padding:30px;
-          font-size: 10px;
-          display: inline-block;                
-         }            
-  </style>
-  <style type="text/css">
-     td { padding: 2px 50px 2px 10px; } <!-- top, right, bottom, left -->
-   </style>
-        
-    <body id="page1">
+ 
+<style>
+#nav {
+  line-height:30px;
+  height:800px;
+  width:100px;
+  float:left;
+  padding:30px;
+  font-size: 10px;
+  display: inline-block;                
+}   
+.newspaper {
+  column-count: 2;
+}
+</style>
+<style type="text/css">
+  td { padding: 2px 50px 2px 10px; } <!-- top, right, bottom, left -->
+</style>
+</head>      
+<body id="page1">
     <!-- header -->
     <!-- this do nothing
     <div class="bg">
     <div class="main">
     -->
-    <header>
+<!--    <header> -->
   <!-- This did nothing   <div class="row-1">  -->
           
-    <h1><center><a class="logo" >OHMBROO WEATHER CAM NORTH HDG 120 - <a href="index.php">CLICK HERE TO UPDATE PIC</a></center>
-      <center><table>
-        <td><a class="logo" target="_blank" rel="noopener noreferrer"                   
-                href="https://tempestwx.com/station/81383/">OHMBROO WEATHER STATION</a></td>
-                <td><a class=""logo" target="_blank" rel="noopener noreferrer"
-                href="./snapshot/2024-10-20-CamsSelfie1.jpg">OHMBROO CAMS SELFIE</a></td>
+<h1><center><a class="logo" >OHMBROO WEATHER CAM NORTH HDG 120 - <a href="index.php">CLICK HERE TO UPDATE PIC</a></center>
+  <center><table>
+    <td><a class="logo" target="_blank" rel="noopener noreferrer"                   
+           href="https://tempestwx.com/station/81383/">OHMBROO WEATHER STATION</a></td>
+    <td><a class=""logo" target="_blank" rel="noopener noreferrer"
+           href="./snapshot/2024-10-20-CamsSelfie1.jpg">OHMBROO CAMS SELFIE</a></td>
                 <!-- src="snapshot/2024-10-20-CamsSelfie1.jpg" -->
-      </table></center>
-        
-    </h1>    
-  
-  
+  </table></center>
+</h1>    
   <!--
   <h1>
         <center><table>
@@ -81,7 +79,7 @@ and open the template in the editor.
         </table></center>
       </h1>
   -->    
-        <center><img src="snapshot/ohmbrooCAM_image.jpg" alt="ohmbrooCAM1"></center>
+<center><img src="snapshot/ohmbrooCAM_image.jpg" alt="ohmbrooCAM1"></center>
                 <!-- webcam.io widget --
                 <h1><center>
                   <script type="text/javascript">window.timelapseview_options=window.timelapseview_options || 
@@ -89,54 +87,45 @@ and open the template in the editor.
                   <script src="https://webcam.io/s/widget-v1_03.load.js?i=EvgEt" async></script>
                 </center></h1>  
                  end webcam.io widget -->   
-      <h2>
-        <center><table>
-          <td><a class="logo" target="_blank" rel="noopener noreferrer" 
-                  href="http://webcam.io/webcams/dzoG2z/">OHMBROO WEATHER CAM TIME LAPSE DASHBOARD</a></td>
-          <td><a class="logo" target="_blank" rel="noopener noreferrer" 
-                 href="http://www.youtube.com/user/ohmbroo/">YOUTUBE CHANNEL - DAILY TIME LAPSE VIDEOS</a></td>
-        </table></center> 
-      </h2>
-      <h2>
-        <center><table>
-          <td><a class="logo" target="_blank" rel="noopener noreferrer" 
-              <a href="http://www.wunderground.com/dashboard/pws/KNMRIORA349#history">WEATHER UNDERGROUND STATION - TEMPEST</a></td>                           
-          <td><a class="logo" target="_blank" rel="noopener noreferrer" 
-              <a href="http://www.wunderground.com/dashboard/pws/KNMRIORA59#history">WEATHER UNDERGROUND STATION - AMBIENT STATION</a></td>
-        </table></center>
-      </h2>
-      <h2>
-        <center><table>  
-          <td><a class="logo" target="_blank" rel="noopener noreferrer" 
-                 href="https://ambientweather.net/dashboard/3abca0ac00a626a6106dcbbe6ac4a441/graphs">AMBIENT WEATHER STATION - SEMI-RETIRED</a></td>              
-          <td><a class="logo" target="_blank" rel="noopener noreferrer" 
-                 <a href="http://www.youtube.com/user/ohmbroo/">LIVESTREAM WX CAM - TBD </a>
-                 <!--<a href="https://www.youtube.com/channel/UCQYvjlZeNZ8KgEmJx58ES6w/live">LIVESTREAM WX CAM - TBD </a> -->
-            </table></center>
-                               
-             <!--   <h1><center><a class="logo" href="gallery.php">KATRINA GALLERY [limited]</a></center> 
-             -->
-      </h2>
-  </body>
- <table> 
+<h2>
+<center><table>
+            
+  <td><a class="logo" target="_blank" rel="noopener noreferrer" 
+         href="http://webcam.io/webcams/dzoG2z/">OHMBROO WEATHER CAM TIME LAPSE DASHBOARD</a></td>
+  <td><a class="logo" target="_blank" rel="noopener noreferrer" 
+         href="http://www.youtube.com/user/ohmbroo/">YOUTUBE CHANNEL - DAILY TIME LAPSE VIDEOS</a></td>
        
-  
-        <tfoot align="center"> 
-            <tr> 
-                <td>Author - Edward Sadzewicz</td> 
-                <td>Updated - 29-DEC-2024</td> 
-                
-            </tr> 
-        </tfoot> 
-    </table> 
-  <!--<tfoot align="right">
-     <p>Author: Edward Sadzewicz - Updated 27-MAY-2024</p> 
-  </tfoot>
-  <!--
-  <footer>
-  <p>Author: Edward Sadzewicz - Updated 27-MAY-2024</p>
+</table></center> 
+</h2>
+<h2>
+<center><table>
+  <td><a class="logo" target="_blank" rel="noopener noreferrer" 
+      <a href="http://www.wunderground.com/dashboard/pws/KNMRIORA349#history">WEATHER UNDERGROUND STATION - TEMPEST</a></td>                           
+  <td><a class="logo" target="_blank" rel="noopener noreferrer" 
+      <a href="http://www.wunderground.com/dashboard/pws/KNMRIORA59#history">WEATHER UNDERGROUND STATION - AMBIENT STATION</a></td>
+</table></center>
+</h2>
+<h2>
+<center><table>  
+  <td><a class="logo" target="_blank" rel="noopener noreferrer" 
+          href="https://ambientweather.net/dashboard/3abca0ac00a626a6106dcbbe6ac4a441/graphs">AMBIENT WEATHER STATION - SEMI-RETIRED</a></td>              
+  <td><a class="logo" target="_blank" rel="noopener noreferrer" 
+      <a href="http://www.youtube.com/user/ohmbroo/">LIVESTREAM WX CAM - TBD </a>
+  <!--<a href="https://www.youtube.com/channel/UCQYvjlZeNZ8KgEmJx58ES6w/live">LIVESTREAM WX CAM - TBD </a> -->
+</table></center>
+    <!--   <h1><center><a class="logo" href="gallery.php">KATRINA GALLERY [limited]</a></center> -->
+    
+</h2>
+<table>
+  <tr> 
+    <td>Author - Edward Sadzewicz</td> 
+    <td>ohmbroo@gmail.com</td>                 
+    <td>Updated - 29-DEC-2024</td>
+  </tr> 
+</table>
   <!--<p><a href="mailto:hege@example.com">hege@example.com</a></p> 
 </footer> 
 -->
-  </header>    
+  <!-- </header>    -->
+</body>
 </html>
